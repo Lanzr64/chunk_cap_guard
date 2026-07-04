d48663d82ef8fc5b0ea2eff33a8134b08ecac65b
Lanzr <mageman@126.com>
2026-07-04 17:59:26 +0800

    降低forge版本向上适配

--- src/main/java/net/lanzr/chunkcapguard/ChunkCapGuard.java
+++ src/main/java/net/lanzr/chunkcapguard/ChunkCapGuard.java
@@ -7,6 +7,7 @@ import net.minecraft.network.protocol.game.ClientboundCommandsPacket;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -15,15 +16,17 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ChunkCapGuard {
     public static final String MODID = "chunkcapguard";
 
-    public ChunkCapGuard(FMLJavaModLoadingContext context) {
+//    public ChunkCapGuard(FMLJavaModLoadingContext context) {
+        public ChunkCapGuard() {
+            IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        IEventBus modEventBus = context.getModEventBus();
+//        IEventBus modEventBus = context.getModEventBus();
         modEventBus.register(this);
 
         // Register Forge events on the Forge event bus (RegisterCommandsEvent is not an IModBusEvent)
         MinecraftForge.EVENT_BUS.addListener(this::onRegisterCommands);
 
-        context.registerConfig(ModConfig.Type.SERVER, Config.SPEC, "chunkcapguard.toml");
+        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, Config.SPEC, "chunkcapguard.toml");
     }
 
     public void onRegisterCommands(RegisterCommandsEvent event) {
